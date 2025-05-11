e79e84efd33ad8af7583222c7fc2661a20837f31
Roger Dingledine <arma@torproject.org>
2025-05-11 17:29:13 -0400

    trivial whitespace formatting fixes

--- src/feature/nodelist/nodelist.c
+++ src/feature/nodelist/nodelist.c
@@ -1507,13 +1507,13 @@ int
 node_is_dir(const node_t *node)
 {
   if (node->rs) {
-    routerstatus_t * rs = node->rs;
+    routerstatus_t *rs = node->rs;
     /* This is true if supports_tunnelled_dir_requests is true which
      * indicates that we support directory request tunnelled or through the
      * DirPort. */
     return rs->is_v2_dir;
   } else if (node->ri) {
-    routerinfo_t * ri = node->ri;
+    routerinfo_t *ri = node->ri;
     /* Both tunnelled request is supported or DirPort is set. */
     return ri->supports_tunnelled_dir_requests;
   } else {
